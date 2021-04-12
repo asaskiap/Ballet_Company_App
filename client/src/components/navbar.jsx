9c477459c1f7f725798735a59ec34f1410771d97
asaskiap <alannapfeiffer@gmail.com>
2021-04-12 20:35:04 +0200

    more mobile styling

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,43 +1,94 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.scss';
-const Navbar = ({ user, onSignOut }) => {
-  return (
-    <nav className="navbar">
-      <Link to="/" className="navLink">
-        <strong>Ballet Company App</strong>
-      </Link>
+import hamburger from './../views/photos/list.png';
 
-      <div>
-        {(user && (
-          <>
-            <Link to={`/${user._id}`} className="navLink">
-              {user.name}
-            </Link>
-            <Link to="/order" className="navLink">
-              Place an Order
-            </Link>
-            {user.isAdministrator && (
-              <Link to="/list" className="navLink">
-                See all Orders
+class Navbar extends React.Component {
+  state = {
+    navDisplay: false
+  };
+
+  toggleNavbar = () => {
+    this.setState({
+      navDisplay: !this.state.navDisplay
+    });
+  };
+  render() {
+    return (
+      <nav className="navbar">
+        <div className="navHamburger">
+          <button onClick={this.toggleNavbar}>
+            {' '}
+            <img src={hamburger} alt="ham" height="40" />
+          </button>
+          <Link to="/" className="navTitle">
+            <strong>Ballet Company App</strong>
+          </Link>
+        </div>
+
+        <div className="navLinkContainer">
+          {(this.props.user && (
+            <>
+              <Link
+                to={`/${this.props.user._id}`}
+                className={` ${
+                  this.state.navDisplay ? 'navDisplay' : 'navLink'
+                }`}
+              >
+                {this.props.user.name}
               </Link>
-            )}
+              <Link
+                to="/order"
+                className={` ${
+                  this.state.navDisplay ? 'navDisplay' : 'navLink'
+                }`}
+              >
+                Place an Order
+              </Link>
+              {this.props.user.isAdministrator && (
+                <Link
+                  to="/list"
+                  className={` ${
+                    this.state.navDisplay ? 'navDisplay' : 'navLink'
+                  }`}
+                >
+                  See all Orders
+                </Link>
+              )}
 
-            <button onClick={onSignOut}>Sign Out</button>
-          </>
-        )) || (
-          <>
-            <Link to="/sign-in" className="navLink">
-              Sign In
-            </Link>
-            <Link to="/sign-up" className="navLink">
-              Sign Up
-            </Link>
-          </>
-        )}
-      </div>
-    </nav>
-  );
-};
+              <button
+                onClick={this.props.onSignOut}
+                className={` ${
+                  this.state.navDisplay ? 'navDisplay' : 'navLink'
+                }`}
+              >
+                Sign Out
+              </button>
+            </>
+          )) || (
+            <>
+              <Link
+                to="/sign-in"
+                className={` ${
+                  this.state.navDisplay ? 'navDisplay' : 'navLink'
+                }`}
+              >
+                Sign In
+              </Link>
+              <Link
+                to="/sign-up"
+                className={` ${
+                  this.state.navDisplay ? 'navDisplay' : 'navLink'
+                }`}
+              >
+                Sign Up
+              </Link>
+            </>
+          )}
+        </div>
+      </nav>
+    );
+  }
+}
 
 export default Navbar;
