64570840c3f8dae2f6d9d8609378f1bc85404a07
asaskiap <alannapfeiffer@gmail.com>
2021-04-17 14:20:54 +0200

    trial email

--- server/utilities/send-weekly-email.js
+++ server/utilities/send-weekly-email.js
@@ -5,20 +5,21 @@ const RECEIVER = 'alannapfeiffer@gmail.com';
 const Order = require('./../models/order');
 
 const sendWeeklyEmail = async() => {
-    const orders = await Order.find({});
-    console.log('orders', orders);
-    const EmailBody = orders.map((order) => {
-        return `<p key=${order._id}>
-        <span> From: ${order.creator_name}</span>
-        <span>Item: ${order.item}</span>
-        <span>Brand: ${order.brand}</span>
-        <span>
-          Size | Width : ${order.size} | {order.width}
-        </span>
-        <span>Quantity: ${order.quantity}</span>
-        <span>Comments: ${order.comments}</span>
-      </p>`;
-    });
+    // const orders = await Order.find({});
+    // console.log('orders', orders);
+    // const EmailBody = orders.map((order) => {
+    //     return `<p key=${order._id}>
+    //     <span> From: ${order.creator_name}</span>
+    //     <span>Item: ${order.item}</span>
+    //     <span>Brand: ${order.brand}</span>
+    //     <span>
+    //       Size | Width : ${order.size} | {order.width}
+    //     </span>
+    //     <span>Quantity: ${order.quantity}</span>
+    //     <span>Comments: ${order.comments}</span>
+    //   </p>`;
+    // });
+    const EmailBody = `<div> This is a trial email</div>`;
     console.log(EmailBody);
 
     await sendEmail({
